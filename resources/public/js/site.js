e5964216fd009239d1494c7cfcb7888f4f6b374d
Yogthos <dmitri.sotnikov@gmail.com>
2012-09-03 02:23:14 -0400

    code cleanup

--- resources/public/js/site.js
+++ resources/public/js/site.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){	
 	var context = $('#context').val();
-    var url = context ? context + '/get-logs' : '/get-logs';
+    var url = '/get-logs';
+    if (context) url = context + url;
     var options = {xaxis: { mode: "time", minTickSize: [1, "minute"]}};
 	$.post(url, function(data){
 	    $.plot($("#hits-by-time"), [data], options);
